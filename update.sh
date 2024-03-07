dab83c637e48a7c330071c4ae7462cacb303c5e1
davidhieber <david.hieber@uni-tuebingen.de>
2024-03-07 15:38:39 +0100

    feat: move flame api into backround thread (Alexander Röhl, David Hieber)

--- update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build  -t flame_test:latest .
-docker tag flame_test:latest dev-harbor.personalhealthtrain.de/flame_test/flame-test:latest
-docker push dev-harbor.personalhealthtrain.de/flame_test/flame-test:latest
