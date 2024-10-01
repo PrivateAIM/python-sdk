77b4597c499aa25d21ca3357e807b69482344871
Nightknight3000 <alexander.roehl@uni-tuebingen.de>
2024-10-01 14:23:49 +0200

    refactor: delete to_be_deleted
    Co-authored-by: antidodo <albin2993@gmail.com>

--- to_be_deleted/flame/utils/envs.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def get_envs() -> dict[str, str]:
-    return {'KEYCLOAK_TOKEN': os.getenv('KEYCLOAK_TOKEN'),
-            'DATA_SOURCE_TOKEN': os.getenv('DATA_SOURCE_TOKEN'),
-            'NGINX_NAME': f'nginx-{os.getenv("DEPLOYMENT_NAME")}'}
-
