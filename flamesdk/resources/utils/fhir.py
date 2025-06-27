ae8a8ac8642028eee9e0bfc4e588f318eb319d04
Nightknight3000 <alexander.roehl@uni-tuebingen.de>
2025-06-27 15:32:04 +0200

    feat: implement simple fhir data flattening
    Co-authored-by: antidodo <albin2993@gmail.com>

---
+++ flamesdk/resources/utils/fhir.py
@@ -0,0 +1,70 @@
+from io import StringIO
+from typing import Optional, Any
+
+# import fhir-kindling
+
+
+def fhir_to_csv(fhir_data: dict[str, Any],
+                col_key_seq: str,
+                row_key_seq: str,
+                value_key_seq: str,
+                row_id_filters: Optional[list[str]] = None,
+                col_id_filters: Optional[list[str]] = None,
+                row_col_name: str = '',
+                separator: str = ',') -> dict[Any, dict[Any, Any]]:
+    df_dict = {}
+
+    for i, entry in enumerate(fhir_data['entry']):
+        col_id = _search_in_fhir_entry(entry, key_sequence=col_key_seq)
+        row_id = _search_in_fhir_entry(entry, key_sequence=row_key_seq)
+        if row_id_filters is not None:
+            if (row_id is None) or (not all([row_id_filter in row_id for row_id_filter in row_id_filters])):
+                continue
+        elif col_id_filters is not None:
+            if (col_id is None) or (not all([col_id_filter in col_id for col_id_filter in col_id_filters])):
+                continue
+        if col_id not in df_dict.keys():
+            df_dict[col_id] = {}
+        if row_id not in df_dict[col_id].keys():
+            df_dict[col_id][row_id] = ''
+
+        value = _search_in_fhir_entry(entry, key_sequence=value_key_seq)
+        df_dict[col_id][row_id] = value
+
+    return df_dict
+
+
+def _search_in_fhir_entry(fhir_entry: dict[str, Any] | list[Any],
+                          key_sequence: str,
+                          current: int = 0) -> Optional[Any]:
+    keys = key_sequence.split('.')
+    key = keys[current]
+    if current < (len(keys) - 1):
+        if type(fhir_entry) == dict:
+            for field in fhir_entry.keys():
+                try:
+                    if field == key:
+                        fhir_entry = fhir_entry[key]
+                        next = _search_in_fhir_entry(fhir_entry, key_sequence, current + 1)
+                        if next is not None:
+                            return next
+                except KeyError:
+                    print(f"KeyError: Unable to find field '{key}' in fhir data at level={current + 1} "
+                          f"(keys found: {fhir_entry.keys()})")
+                    return None
+        elif type(fhir_entry) == list:
+            for e in fhir_entry:
+                next = _search_in_fhir_entry(e, key_sequence, current)
+                if next is not None:
+                    return next
+    else:
+        if current == (len(keys) - 1):
+            if type(fhir_entry) == dict:
+                return fhir_entry[key]
+            elif type(fhir_entry) == list:
+                for e in fhir_entry:
+                    next = _search_in_fhir_entry(e, key_sequence, current)
+                    if next is not None:
+                        return next
+        else:
+            print(f"Unexpected data type found (found type={type(fhir_entry)})")
--- tests/unit_test/test_util.py
+++ tests/unit_test/test_util.py
@@ -1,5 +1,7 @@
-from flamesdk.resources.utils import extract_remaining_time_from_token , flame_log
-
+from flamesdk.resources.utils.fhir import fhir_to_csv, _search_in_fhir_entry
+from flamesdk.resources.utils.utils import extract_remaining_time_from_token, flame_log
+import ast
+import time
 
 def test_extract_remaining_time_from_token():
     token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJBWWdqdWV5T09pRVZjM2pOYVdweWtZWWptQjhOaEVkTTRlbFhzUHN5SDhvIn0.eyJleHAiOjE3NDE2ODYxMzcsImlhdCI6MTc0MTY4NDMzNywianRpIjoiMzUwZWM2ZWUtMWYwMi00NDdhLTgyOWYtOTE5MzMxODNhNGY1IiwiaXNzIjoiaHR0cDovL2ZsYW1lLW5vZGUta2V5Y2xvYWsvcmVhbG1zL2ZsYW1lIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjIzYzI5ODQxLThhNzUtNGFjNi1hMjM4LTAyN2QyMDJjN2FjYyIsInR5cCI6IkJlYXJlciIsImF6cCI6ImJiOWFhMTY1LTkwOTYtNGFhZS1iNTE5LTgxN2Y4NTdlNjNiYSIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLWZsYW1lIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6ImVtYWlsIHByb2ZpbGUiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImNsaWVudEhvc3QiOiIxMC4yNDQuMTc0LjI0NiIsInByZWZlcnJlZF91c2VybmFtZSI6InNlcnZpY2UtYWNjb3VudC1iYjlhYTE2NS05MDk2LTRhYWUtYjUxOS04MTdmODU3ZTYzYmEiLCJjbGllbnRBZGRyZXNzIjoiMTAuMjQ0LjE3NC4yNDYiLCJjbGllbnRfaWQiOiJiYjlhYTE2NS05MDk2LTRhYWUtYjUxOS04MTdmODU3ZTYzYmEifQ.o-jq4eMASfwigw83k5XWpaGrl1_omUNP9onkGqa1LWhY_j8Ziv45A4c1IUjcCdSBBXMwylFoNxvA97lHKHsOFH5Bv3EeVDeIUA3YyCPFPyVAH8Woi26E0iGTmUoFyW8Vn6_Xk_jRfK280BHORL6SxjH5nvGQuVIkXHCgaTo2YTRN4ze4i1xpCnNwBcdC7y94y5MrVT9xDGalgB7qfho0lIGzdgXNJjwBwnDXRjrszShsvkW2TCphql0kS7pEMDptWd2WHavIHAQqQritFfe5VylEdhkH2u_FeNksESAZJlYHPxNSz1XWYtDLymnFw_oQbOF_kf0PI_d4-gJ96W8h2g"
@@ -12,3 +14,20 @@ def test_flame_log():
         file_content = file.read()
     flame_log(file_content, False)
     flame_log("file_content", True, suppress_head=True)
+
+def test_multi_param():
+    filename = "stream_observation.json"
+    with open(filename, "r") as f:
+        content = f.read()
+        fhir_data = ast.literal_eval(content)
+        return fhir_to_csv(fhir_data[0],
+                           col_key_seq="resource.subject.reference",
+                           row_key_seq="resource.component.valueCodeableConcept.coding.code",
+                           value_key_seq="resource.component.valueQuantity.value",
+                           row_id_filters=["ENSG"])
+
+start_time = time.time()
+#print(_search_in_fhir_entry({'fullUrl': 'http://nginx-analysis-671b3985-f901-48c5-9cba-ee62bc6f393d-1/fhir/Observation/gene-observation-C00039-ENSG00000005156', 'resource': {'category': [{'coding': [{'code': 'laboratory', 'display': 'Laboratory', 'system': 'http://terminology.hl7.org/CodeSystem/observation-category'}]}], 'code': {'coding': [{'code': '69548-6', 'display': 'Genetic variant assessment', 'system': 'http://loinc.org'}]}, 'component': [{'code': {'coding': [{'code': '48018-6', 'display': 'Gene studied ' '[ID]', 'system': 'http://loinc.org'}]}, 'valueCodeableConcept': {'coding': [{'code': 'ENSG00000005156', 'system': 'http://ensembl.org'}]}}, {'code': {'coding': [{'code': '48003-8', 'display': 'DNA sequence ' 'variation ' 'identifier ' '[Identifier]', 'system': 'http://loinc.org'}]}, 'valueQuantity': {'code': 'count', 'system': 'http://unitsofmeasure.org', 'unit': 'count', 'value': 2452}}], 'id': 'gene-observation-C00039-ENSG00000005156', 'meta': {'lastUpdated': '2025-06-12T11:24:08.247Z', 'versionId': '3'}, 'resourceType': 'Observation', 'status': 'final', 'subject': {'reference': 'Patient/C00039'}}, 'search': {'mode': 'match'}}, 'resource.component.valueQuantity.value' ))
+print(test_multi_param())
+print(f"Elapsed time: {time.time() - start_time} secs")
+print(f"Estimated time: {(time.time() - start_time) * ((41886 * 118) / 500) / 60} minutes")
